b09f14fabe0cf429ec13932a7327cb7fc3e3e684
eng-muhammedabdulrahman <eng-muhammed.abdulrahman@outlook.com>
2023-06-18 23:32:46 +0300

    updata code

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,85 @@ let orderRange = Array.from(Array(blocks.length).keys());
 
 // Add Order Css Property To Game Blocks
 blocks.forEach((block, index) => {
+
+  // Add CSS Order Property
   block.style.order = orderRange[index];
+
+  // Add Click Event
+  block.addEventListener('click', function(){
+
+    // Trigger The Flip Block Function
+    flipBlock(block);
+  });
+
 });
+
+// Flip Block Function
+function flipBlock(selectedBlock) {
+
+  // Add Class is-flipped
+  selectedBlock.classList.add('is-flipped');
+
+  // Collect All Flipped Cards
+  let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'));
+
+  // If Theres Two Selected Blocks
+  if (allFlippedBlocks.length === 2) {
+
+    // Stop Clicking Function
+    stopClicking();
+
+    // Check Matched Block Function
+    checkMatchedBlocks(allFlippedBlocks[0], allFlippedBlocks[1]);
+
+  }
+
+}
+
+// Stop Clicking Function
+function stopClicking() {
+
+  // Add Class No Clicking on Main Container
+  blocksContainer.classList.add('no-clicking');
+
+  setTimeout(() => {
+
+    // Remove Class No Clicking After The Duration
+    blocksContainer.classList.remove('no-clicking');
+
+  }, duration);
+}
+
+// Check Matched Block
+function checkMatchedBlocks(firstBlock, secondBlock) {
+
+  let triesElement = document.querySelector('.tries span');
+
+  if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+
+    firstBlock.classList.remove('is-flipped');
+    secondBlock.classList.remove('is-flipped');
+
+    secondBlock.classList.add('has-match');
+    secondBlock.classList.add('has-match');
+
+    document.getElementById('success').play();
+
+  }else{
+    triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
+
+    setTimeout(() => {
+
+      firstBlock.classList.remove('is-flipped');
+      secondBlock.classList.remove('is-flipped');
+
+    }, duration);
+
+    document.getElementById('fail').play();
+    
+  }
+
+}
 
 // Shuffle Function
 function shuffle(array) {
@@ -51,8 +128,25 @@ function shuffle(array) {
 
     // Decrease Length By One
     current--;
+
+    // [1] Save Current Element in Stash
+    temp = array[current];
+
+    // [2] Current Element = Random Element
+    array[current] = array[random];
+
+    // [3] Random Element = Get Element From Stash
+    array[random] = temp;
   }
+
+  return array;
 
 }
 
-// Array []
+// Current Array [9,2,10,4,5,6,7,3,1,8]
+/* 
+[1] Save Current Element in Stash
+[2] Current Element = Random Element
+[3] Random Element = Get Element From Stash
+*/
+
